d9de0fcacbad3128eaf9346c28bcb4e1061b3156
DJ Mosca <djmosc@gmail.com>
2014-09-28 18:05:17 +0100

    Shopping

--- functions.php
+++ functions.php
@@ -30,6 +30,14 @@ add_action( 'wp_print_styles', 'custom_styles', 30);
 
 remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
 
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
+
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30);
+
+remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5);
+
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+
 // Custom Filters
 
 add_filter( 'woocommerce_enqueue_styles', '__return_false' );
