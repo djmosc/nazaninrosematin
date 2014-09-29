d533adac010daae7da3be1edca36838b2a1982e2
DJ Mosca <djmosc@gmail.com>
2014-09-29 03:08:43 +0100

    Moved page layout a bit

--- page.php
+++ page.php
@@ -1,22 +1,15 @@
 
 <?php get_header(); ?>
 
-<div id="page" >
-
+<section id="page" >
 <?php while ( have_posts() ) : the_post(); ?>
-	<?php 
-		$class = array();
-		$class[] = (has_post_thumbnail()) ? 'has-featured-image' : 'no-featured-image';
-		$class[] = 'single';
-	?>
-	<article id="post-<?php the_ID(); ?>" <?php post_class(implode(' ', $class)); ?>>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		
-		<?php get_template_part('inc/page-header'); ?>
+		<?php get_template_part('inc/post-header'); ?>
 
 		<?php get_template_part('inc/post-content'); ?>
 
 	</article>
 <?php endwhile; // end of the loop. ?>
-
-</div><!-- #page -->
+</section><!-- #page -->
 <?php get_footer(); ?>
